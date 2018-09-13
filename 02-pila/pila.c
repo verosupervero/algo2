01c528e46226c351c0288324b66b54c174685e6c
Fernando Danko <fernandodanko@gmail.com>
2018-09-13 19:24:51 -0300

    gitignore

---
+++ 02-pila/pila.c
@@ -0,0 +1,16 @@
+#include "pila.h"
+#include <stdlib.h>
+
+/* Definición del struct pila proporcionado por la cátedra.
+ */
+struct pila {
+    void** datos;
+    size_t cantidad;  // Cantidad de elementos almacenados.
+    size_t capacidad;  // Capacidad del arreglo 'datos'.
+};
+
+/* *****************************************************************
+ *                    PRIMITIVAS DE LA PILA
+ * *****************************************************************/
+
+// ...
