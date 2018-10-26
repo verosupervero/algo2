0e74759e9068ae4798829b7b204af535f311736b
Fernando Danko <fernandodanko@gmail.com>
2018-10-26 00:43:23 -0300

    arregla grep

--- tp1/grep.c
+++ tp1/grep.c
@@ -1,15 +1,49 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "procesar_subcadena.h"
 
-int main (int argc, char * argv []){
-  if(argc!=3 || argc!=4){
-    fprintf(stderr, "%s\n", "Cantidad de parámetros errónea");
-    return 1;
+/*
+Modos de invocación
+
+./programa <string> <nro lineas>  # lee de stdin
+./programa <string> <nro lineas> <archivo>  # lee de archivo
+*/
+
+void mostrar_ayuda(char * prog_name){
+  if (prog_name == NULL){
+    prog_name = "./grep";
   }
+
+  printf("%s imprime las lineas de un archivo que contengan la cadena indicada\n\
+  \n\
+  Modos de invocación:\n\
+  \t%s <string> <nro lineas>  # lee de stdin\n\
+  \t%s <string> <nro lineas> <archivo>  # lee de archivo\n\
+  \n\
+  El programa devolvera las las lineas que contengan <string>, precedida por las\n\
+  <nro lineas> anteriores y posteriores a cada match.\n\
+  \n\
+  Si no se especifica <archivo> se lee de stdin.\n", prog_name, prog_name, prog_name);
+}
+
+int main (int argc, char * argv []){
+  // Mostrar ayuda
+  if(argc >1 && !strcmp(argv[1], "--help")){
+    mostrar_ayuda(argv[0]);
+    exit(0);
+  }
+
+  // Validacion cantidad parametros
+  if(argc!=3 && argc!=4){
+    fprintf(stderr, "%s\n", "Cantidad de parámetros errónea");
+    exit(1);
+  }
+
+  // El segundo parámetro debe ser un numero
   if(atoi(argv[2])<0){
     fprintf(stderr, "%s\n","Tipo de parámetro incorrecto");
-    return 1;
+    exit(1);
   }
 
   char * subcadena= argv[1];
@@ -21,13 +55,18 @@ int main (int argc, char * argv []){
     archivo= fopen(nombre_archivo, "r");
     if(!archivo){
       fprintf(stderr, "%s\n","No se pudo leer el archivo indicado");
-      return 1;
+      exit(1);
     }
   }
-  else{ archivo= stdin; }
+  else{
+    archivo= stdin;
+  }
+
+  fprintf(stderr, "Lineas: %d, string: %s\n", cant_lineas, subcadena);
 
   bool mostro=mostrar_subcadena_en_archivo(subcadena,(size_t)cant_lineas,archivo);
+
   if(argc==4)
     fclose(archivo);
-  return !mostro;
+  exit(!mostro);
 }
