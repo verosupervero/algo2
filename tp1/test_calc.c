b2ee5c61bf6012976a73a9c6d1cd44d6a3e1baf1
Poshito Nandu desde el celu <fdanko@fi.uba.ar>
2018-10-26 19:38:48 -0300

    agregado test para parseon en calc

--- tp1/test_calc.c
+++ tp1/test_calc.c
@@ -14,5 +14,15 @@ int main (){
 	print_test("2+3 retorno ok", procesar_vector_polaco_inverso(vector, &resultado));
 	print_test("2+3 resultado 5", resultado == 5);
 	
+	puts("Probando parseo");
+	char* linea = " 2  3\t +  log\n";
+	char** tokens = parsear_literales(linea, ' ');
+ 
+ char* tokens_ok = {"2", "3", "+", "log", NULL};
+ print_test("largo vector string", strv_len(tokens)==4);
+ for (size_t i=0; tokens_ok[i]!=NULL; i++){
+   print_test(tokens[i], !strcmp(tokens[i], tokens_ok[i]) && !strcmp(tokens_ok[i], tokens[i]));
+ }
+ 
 	return 0;
 }
